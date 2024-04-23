95f352cd439a547f2fb154f957b8b6928ed01ea7
Виктор Столяров <accordvitek@mail.ru>
2024-04-23 10:22:01 +0300

    add task 01-04

---
+++ src/lab3/task01/Study.java
@@ -0,0 +1,13 @@
+package lab3.task01;
+
+public class Study {
+    private String course;
+
+    public Study() {
+        course = "Изучение Java - это просто";
+    }
+
+    public String printCourse() {
+        return this.course;
+    }
+}
